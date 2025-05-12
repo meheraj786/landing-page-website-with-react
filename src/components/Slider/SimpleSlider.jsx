3ad265b3926065461ebb259314b3b55462f14355
Mehraj Hosen <meherajhosen786@gmail.com>
2025-05-13 00:57:34 +0600

    slider bug fix

--- src/components/Slider/SimpleSlider.jsx
+++ src/components/Slider/SimpleSlider.jsx
@@ -15,10 +15,72 @@ export default function SimpleSlider() {
     autoplay: true,
     autoplaySpeed: 3000
   };
+  const format = [
+    {
+      content: (
+        <div className="h-[426px] pt-[70px]">
+          <div className="review relative rounded-[20px] bg-white p-[50px]">
+            <div className="circle w-[124px] h-[124px] bg-[#C4C4C4] border-10 border-[#F7F7FC] absolute top-[-80px] z-10 left-[48px] rounded-full"></div>
+            <p className="text-description text-[22px] leading-[44px] w-[567px]">
+              “Wow. Amazing company amazing Expert Manager. They know how to get things done when it comes to online trading. 1
+            </p>
+  
+            <Flex className="justify-between mt-[37px]">
+              <div>
+                <h3 className="text-6 text-text font-bold font-primary leading-[36px] mb-[4px]">Robert Fox</h3>
+                <p className="text-4 text-description font-regular font-secondary leading-[26px]">Digital Marketer</p>
+              </div>
+              <img src={stars} alt="Rating stars" />
+            </Flex>
+          </div>
+        </div>
+      ),
+    },
+    {
+      content: (
+        <div className="h-[426px] pt-[70px]">
+          <div className="review relative rounded-[20px] bg-white p-[50px]">
+            <div className="circle w-[124px] h-[124px] bg-[#C4C4C4] border-10 border-[#F7F7FC] absolute top-[-80px] z-10 left-[48px] rounded-full"></div>
+            <p className="text-description text-[22px] leading-[44px] w-[567px]">
+              “Wow. Amazing company amazing Expert Manager. They know how to get things done when it comes to online trading. 2
+            </p>
+  
+            <Flex className="justify-between mt-[37px]">
+              <div>
+                <h3 className="text-6 text-text font-bold font-primary leading-[36px] mb-[4px]">Robert Fox</h3>
+                <p className="text-4 text-description font-regular font-secondary leading-[26px]">Digital Marketer</p>
+              </div>
+              <img src={stars} alt="Rating stars" />
+            </Flex>
+          </div>
+        </div>
+      ),
+    },
+    {
+      content: (
+        <div className="h-[426px] pt-[70px]">
+          <div className="review relative rounded-[20px] bg-white p-[50px]">
+            <div className="circle w-[124px] h-[124px] bg-[#C4C4C4] border-10 border-[#F7F7FC] absolute top-[-80px] z-10 left-[48px] rounded-full"></div>
+            <p className="text-description text-[22px] leading-[44px] w-[567px]">
+              “Wow. Amazing company amazing Expert Manager. They know how to get things done when it comes to online trading. 3
+            </p>
+  
+            <Flex className="justify-between mt-[37px]">
+              <div>
+                <h3 className="text-6 text-text font-bold font-primary leading-[36px] mb-[4px]">Robert Fox</h3>
+                <p className="text-4 text-description font-regular font-secondary leading-[26px]">Digital Marketer</p>
+              </div>
+              <img src={stars} alt="Rating stars" />
+            </Flex>
+          </div>
+        </div>
+      ),
+    },
+  ];
   return (
     <Slider {...settings}>
-      <div className='relative' >
-                  <div className="review relative rounded-[20px] bg-white p-[60px]">
+      {/* <div className='h-[426px] pt-[70px]' >
+                  <div className="review relative rounded-[20px] bg-white p-[50px]">
             <div className="circle w-[124px] h-[124px] bg-[#C4C4C4] border-10 border-[#F7F7FC] absolute top-[-80px] z-10 left-[48px] rounded-full"></div>
             <p className='text-description text-[22px] leading-[44px] w-[567px]'>“Wow. Amazing company amazing Expert Manager. They know how to get things done when it comes to online trading. </p>
 
@@ -32,7 +94,7 @@ export default function SimpleSlider() {
 
           </div>
       </div>
-      <div>
+      <div className='h-[426px] pt-[70px]' >
                   <div className="review relative rounded-[20px] bg-white p-[60px]">
             <div className="circle w-[124px] h-[124px] bg-[#C4C4C4] border-10 border-[#F7F7FC] absolute top-[-80px] left-[48px] rounded-full"></div>
             <p className='text-description text-[22px] leading-[44px] w-[567px]'>“Wow. Amazing company amazing Expert Manager. They know how to get things done when it comes to online trading. </p>
@@ -47,7 +109,7 @@ export default function SimpleSlider() {
 
           </div>
       </div>
-      <div>
+      <div className='h-[426px] pt-[70px]' >
                   <div className="review relative rounded-[20px] bg-white p-[60px]">
             <div className="circle w-[124px] h-[124px] bg-[#C4C4C4] border-10 border-[#F7F7FC] absolute top-[-80px] left-[48px] rounded-full"></div>
             <p className='text-description text-[22px] leading-[44px] w-[567px]'>“Wow. Amazing company amazing Expert Manager. They know how to get things done when it comes to online trading. </p>
@@ -62,7 +124,7 @@ export default function SimpleSlider() {
 
           </div>
       </div>
-      <div>
+      <div className='h-[426px] pt-[70px]' >
                   <div className="review relative rounded-[20px] bg-white p-[60px]">
             <div className="circle w-[124px] h-[124px] bg-[#C4C4C4] border-10 border-[#F7F7FC] absolute top-[-80px] left-[48px] rounded-full"></div>
             <p className='text-description text-[22px] leading-[44px] w-[567px]'>“Wow. Amazing company amazing Expert Manager. They know how to get things done when it comes to online trading. </p>
@@ -77,7 +139,7 @@ export default function SimpleSlider() {
 
           </div>
       </div>
-      <div>
+      <div className='h-[426px] pt-[70px]' >
                   <div className="review relative rounded-[20px] bg-white p-[60px]">
             <div className="circle w-[124px] h-[124px] bg-[#C4C4C4] border-10 border-[#F7F7FC] absolute top-[-80px] left-[48px] rounded-full"></div>
             <p className='text-description text-[22px] leading-[44px] w-[567px]'>“Wow. Amazing company amazing Expert Manager. They know how to get things done when it comes to online trading. </p>
@@ -92,7 +154,7 @@ export default function SimpleSlider() {
 
           </div>
       </div>
-      <div>
+      <div className='h-[426px] pt-[70px]' >
                   <div className="review relative rounded-[20px] bg-white p-[60px]">
             <div className="circle w-[124px] h-[124px] bg-[#C4C4C4] border-10 border-[#F7F7FC] absolute top-[-80px] left-[48px] rounded-full"></div>
             <p className='text-description text-[22px] leading-[44px] w-[567px]'>“Wow. Amazing company amazing Expert Manager. They know how to get things done when it comes to online trading. </p>
@@ -106,7 +168,12 @@ export default function SimpleSlider() {
             </Flex>
 
           </div>
-      </div>
+      </div> */}
+      {
+        format.map((item)=>(
+          <div>{item.content}</div>
+        ))
+      }
     </Slider>
   );
 }
