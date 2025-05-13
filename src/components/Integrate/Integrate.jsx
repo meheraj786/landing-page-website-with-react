948d9737bf9123715df2d12096c4e7cf68424d88
Mehraj Hosen <meherajhosen786@gmail.com>
2025-05-13 15:26:45 +0600

    css fix

--- src/components/Integrate/Integrate.jsx
+++ src/components/Integrate/Integrate.jsx
@@ -22,16 +22,33 @@ const Integrate = () => {
           <p className='text-[18px] font-secondary font-bold  mb-[40px] leading-[32px] text-description'>App integration, in a general sense, is the process of bringing resources or capabilities from one application to another. As the world of apps continues to evolve, app integration is becoming expected in many contexts.</p>
           <Button className="text-white">Get Started</Button>
         </div>
-        <div className="integrate-right w-[50%]">
-          <Flex className="justify-center">
-          <img className='w-[332px]' src={icon1} alt="" />
-          <img className='w-[332px]' src={icon4} alt="" />
-          <img className='w-[151px]' src={icon8} alt="" />
-          <img className='w-[332px]' src={icon2} alt="" />
-          <img className='w-[151px]' src={icon5} alt="" />
-          <img className='w-[290px]' src={icon3} alt="" />
-          <img className='w-[151px]' src={icon9} alt="" />
-          <img className='w-[196px]' src={icon6} alt="" />
+        <div className="integrate-right w-[697px]">
+          <Flex className="justify-between gap-y-[35px]">
+            <div className='px-[65px] py-[45px] bg-white shadow-md rounded-[12px]'>
+          <img className='w-[202px]' src={icon1} alt="" />
+            </div>
+            <div className='px-[88px] py-[48px] bg-white shadow-md rounded-[12px]'>
+          <img className='w-[155px]' src={icon2} alt="" />
+            </div>
+            <div className="py-[45px] px-[30px] bg-white shadow-md rounded-[12px]">
+          <img className='w-[90px]' src={icon3} alt="" />
+            </div>
+            <div className="px-[80px] py-[50px] bg-white shadow-md rounded-[12px]">
+          <img className='w-[174px]' src={icon4} alt="" />
+            </div>
+            <div className="px-[38px] py-[33px] bg-white shadow-md rounded-[12px]">
+          <img className='w-[76px]' src={icon5} alt="" />
+            </div>
+            <div className="px-[28px] py-[49px] bg-white shadow-md rounded-[12px]">
+          <img className='w-[277px]' src={icon6} alt="" />
+            </div>
+            <div className="px-[38px] py-[33px] bg-white shadow-md rounded-[12px]">
+          <img className='w-[76px]' src={icon8} alt="" />
+            </div>
+            <div className="px-[8px] py-[54px] bg-white shadow-md rounded-[12px]">
+          <img className='w-[166px]' src={icon9} alt="" />
+            </div>
+
 
           </Flex>
 
