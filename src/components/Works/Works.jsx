56c9a50c435ce30b340fcdbf5e6dd1ee314f0b0f
Mehraj Hosen <meherajhosen786@gmail.com>
2025-05-21 00:14:10 +0600

    services done

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -41,11 +41,11 @@ const Works = () => {
     <div className="works py-[140px]">
       <Container>
         <p className='text-orng text-center'>Our Services</p>
-        <h2 className='font-primary text-center text-[46px] mt-[4px] mb-[80px] font-bold  leading-[58px] text-text'>How It Works</h2>
-        <Flex className="justify-between">
+        <h2 className='font-primary text-center md:text-[46px] text-[36px] mt-[4px] mb-[50px] md:mb-[80px] font-bold  leading-[58px] text-text'>How It Works</h2>
+        <Flex className="md:justify-between">
           {
             works.map((work)=>(
-              <div className="group card cursor-pointer hover:bg-primary  border-1 rounded-[20px] text-text border-[#DCE7FE] p-[36px] w-[330px]">
+              <div className="group card cursor-pointer hover:bg-primary mx-auto mb-5 md:mb-0  border-1 rounded-[20px] text-text border-[#DCE7FE] p-[36px] w-[330px]">
                 <img src={work.img} alt="" />
                 <h3 className='font-primary text-[22px] group-hover:text-white mt-[26px] mb-[8px] font-bold  leading-[34px] '>{work.title}</h3>
                 <p className='font-secondary text-[16px] font-regular mb-[26px] leading-[26px] group-hover:text-white text-description'>{work.description}</p>
